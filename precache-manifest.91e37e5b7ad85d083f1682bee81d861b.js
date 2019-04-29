574d111e462c1dda1ad8213dc1491fae1f562122
Akash Arora <arora110@mail.chapman.edu>
2019-04-29 15:42:15 -0700

    Updates

---
+++ precache-manifest.91e37e5b7ad85d083f1682bee81d861b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "65a041bcac40cc15e73cabab86f81e18",
+    "url": "/AspirationBank_Project/static/media/gitCat.65a041bc.png"
+  },
+  {
+    "revision": "b25b6cf2c24e60bca460",
+    "url": "/AspirationBank_Project/static/js/runtime~main.5d294abb.js"
+  },
+  {
+    "revision": "3e1418ad61e23a461328",
+    "url": "/AspirationBank_Project/static/js/main.abbf1c6d.chunk.js"
+  },
+  {
+    "revision": "b167f39e2ce929a2fe4b",
+    "url": "/AspirationBank_Project/static/js/2.1640fb24.chunk.js"
+  },
+  {
+    "revision": "3e1418ad61e23a461328",
+    "url": "/AspirationBank_Project/static/css/main.bbee792a.chunk.css"
+  },
+  {
+    "revision": "a1329500cfb4612096206819e96cfebf",
+    "url": "/AspirationBank_Project/index.html"
+  }
+];
